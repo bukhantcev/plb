29052f921c9e34111440ef311e0671434d452736
buha <bukhantcev@mail.ru>
2024-09-23 16:54:48 +0300

    plb_23.09.24_last

--- plb/config/settings.py
+++ plb/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r2+=gk7or&w==%f_((b5j+!4xprq*lz-9sjv)*$4=(6a^k6&c2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '78.111.88.18:8000']
+ALLOWED_HOSTS = ['127.0.0.1', '78.111.88.18']
 
 
 # Application definition
