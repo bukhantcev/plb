f9a808673453f27e2ff2616e6434cd40556d30cd
buha <bukhantcev@mail.ru>
2024-10-10 10:20:49 +0300

    after deploi

--- plb/plb/settings.py
+++ plb/plb/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'statics'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
