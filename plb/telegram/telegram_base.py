cfde17fe1cafed758de5cbbb0fc1ccde0a228111
buha <bukhantcev@mail.ru>
2024-10-11 11:15:31 +0300

    after deploi 11.10

--- plb/telegram/telegram_base.py
+++ plb/telegram/telegram_base.py
@@ -1,6 +1,7 @@
+import json
 
 import requests
-
+from urllib3 import request
 
 from .input_config import BOT_TOKEN, CHAT_ID
 
@@ -12,10 +13,15 @@ BOT_TOKEN = BOT_TOKEN
 CHAT_ID = CHAT_ID
 
 def sendMessage():
+    url_text = 'http://api.forismatic.com/api/1.0/'
+    data = {'method': 'getQuote', 'format': 'text', 'lang': 'ru'}
+    citata = requests.post(url_text, data=data).text
 
-    msg = "Привет! У тебя новая запись!"
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+    msg = f"{citata}\n<a href='https://pourlabeaute.ru/calendar/jdut-podtvergdeniy'>Новая запись</a>"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&parse_mode=HTML&text={msg}"
     print(requests.get(url).json())
+
+sendMessage()
 
 
 
