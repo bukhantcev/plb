f34c3ce6ac0c549c57952eb36fb54a0997962a18
buha <bukhantcev@mail.ru>
2024-10-24 12:30:09 +0300

    turciya

--- plb/main/views.py
+++ plb/main/views.py
@@ -44,7 +44,7 @@ def home(request):
                }
 
 
-    return render(request, 'main/home.html', context=context)
+    return render(request, 'main/home.html' if request.user.is_staff else 'main/welcome.html', context=context)
 
 #----------------------------ZAPIS BEZ USLUGI
 def zapis(request):
